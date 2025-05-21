f8155fe8ec0b19fdd67e9013a892ebb86bdc3310
Gaurav <92074001+gauravxa137@users.noreply.github.com>
2025-05-21 18:25:21 +0530

    higherlower

---
+++ day14 _HigherLower/game_data.py
@@ -0,0 +1,152 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 266,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count': 95,
+        'description': 'Prime Minister',
+        'country': 'India'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 89,
+        'description': 'Actress and singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Shraddha Kapoor',
+        'follower_count': 84,
+        'description': 'Actress and singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Alia Bhatt',
+        'follower_count': 80,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Deepika Padukone',
+        'follower_count': 78,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Neha Kakkar',
+        'follower_count': 76,
+        'description': 'Singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Katrina Kaif',
+        'follower_count': 74,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Amitabh Bachchan',
+        'follower_count': 68,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'MS Dhoni',
+        'follower_count': 50,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Sachin Tendulkar',
+        'follower_count': 47,
+        'description': 'Cricket legend',
+        'country': 'India'
+    },
+    {
+        'name': 'Janhvi Kapoor',
+        'follower_count': 39,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Kiara Advani',
+        'follower_count': 45,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Flipkart',
+        'follower_count': 3,
+        'description': 'E-commerce platform',
+        'country': 'India'
+    },
+    {
+        'name': 'T-Series',
+        'follower_count': 60,
+        'description': 'Music label and production company',
+        'country': 'India'
+    },
+    {
+        'name': 'Ranveer Singh',
+        'follower_count': 45,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Anushka Sharma',
+        'follower_count': 62,
+        'description': 'Actress and producer',
+        'country': 'India'
+    },
+    {
+        'name': 'Sunny Leone',
+        'follower_count': 57,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Akshay Kumar',
+        'follower_count': 64,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Ratan Tata',
+        'follower_count': 12,
+        'description': 'Industrialist and philanthropist',
+        'country': 'India'
+    },
+    {
+        'name': 'ISRO',
+        'follower_count': 6,
+        'description': 'Space research organization',
+        'country': 'India'
+    },
+    {
+        'name': 'Zomato',
+        'follower_count': 2,
+        'description': 'Food delivery platform',
+        'country': 'India'
+    },
+    {
+        'name': 'Swiggy',
+        'follower_count': 1,
+        'description': 'Food delivery platform',
+        'country': 'India'
+    },
+    {
+        'name': 'Jio',
+        'follower_count': 4,
+        'description': 'Telecommunications company',
+        'country': 'India'
+    }
+]
